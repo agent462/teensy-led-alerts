1900d492a6f5da67cac48d84cfb913c1ec2330e8
@agent462 <agent462@gmail.com>
2013-01-26 19:04:48 -0600

    initial commit of the arduino file to control the LED's

---
+++ teensyledalerts/teensyledalerts.ino
@@ -0,0 +1,170 @@
+#include "LPD8806.h"
+#include "SPI.h"
+
+int dataPin = 2;
+int clockPin = 1;
+int leds = 18;
+char alert;
+char last;
+
+LPD8806 strip = LPD8806(leds, dataPin, clockPin);
+
+void setup() {
+  // Start up the LED strip
+  strip.begin();
+  Serial.begin(9600);
+  // Update the strip, to start they are all 'off'
+  strip.show();
+}
+
+// function prototypes, do not remove these!
+void scanner(uint8_t r, uint8_t g, uint8_t b, uint8_t wait);
+void wave(uint32_t c, int cycles, uint8_t wait);
+void rainbowCycle(uint8_t wait);
+uint32_t Wheel(uint16_t WheelPos);
+
+void loop() {
+  if (Serial.available() > 0) {
+    alert = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(alert);
+  }
+  if (alert == false) {
+    alert = last;
+  }
+
+  switch (alert) {
+    case '1':
+      setColor(strip.Color(0, 127,0));      //all green
+      break;
+    case '2':
+      scanner(127,0,0, 30);                 // red, slow
+      break;
+    case '3':
+      wave(strip.Color(127,0,0), 4, 20);    // red/white/blue
+      break;
+    default:
+      setColor(strip.Color(0, 0,0));
+    }
+  last = alert;
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j=0; j < 384 * 5; j++) {     // 5 cycles of all 384 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 384-color
+      // wheel (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 384 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel(((i * 384 / strip.numPixels()) + j) % 384));
+    }
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+void setColor(uint32_t c) {
+  int i;
+  for(i=0; i < strip.numPixels(); i++){
+    strip.setPixelColor(i, c);
+    strip.show();
+  }
+}
+
+void scanner(uint8_t r, uint8_t g, uint8_t b, uint8_t wait) {
+  int i, j, pos, dir;
+
+  pos = 0;
+  dir = 1;
+
+  for(i=0; i<((strip.numPixels()-1) * 8); i++) {
+    // Draw 5 pixels centered on pos.  setPixelColor() will clip
+    // any pixels off the ends of the strip, no worries there.
+    // we'll make the colors dimmer at the edges for a nice pulse
+    // look
+    strip.setPixelColor(pos - 2, strip.Color(r/4, g/4, b/4));
+    strip.setPixelColor(pos - 1, strip.Color(r/2, g/2, b/2));
+    strip.setPixelColor(pos, strip.Color(r, g, b));
+    strip.setPixelColor(pos + 1, strip.Color(r/2, g/2, b/2));
+    strip.setPixelColor(pos + 2, strip.Color(r/4, g/4, b/4));
+
+    strip.show();
+    delay(wait);
+    for(j=-2; j<= 2; j++) 
+        strip.setPixelColor(pos+j, strip.Color(0,0,0));
+    // Bounce off ends of strip
+    pos += dir;
+    if(pos < 0) {
+      pos = 1;
+      dir = -dir;
+    } else if(pos >= strip.numPixels()) {
+      pos = strip.numPixels() - 2;
+      dir = -dir;
+    }
+  }
+}
+
+#define PI 3.14159265
+void wave(uint32_t c, int cycles, uint8_t wait) {
+  float y;
+  byte  r, g, b, r2, g2, b2;
+
+  // Need to decompose color into its r, g, b elements
+  g = (c >> 16) & 0x7f;
+  r = (c >>  8) & 0x7f;
+  b =  c        & 0x7f; 
+
+  for(int x=0; x<(strip.numPixels()*5); x++)
+  {
+    for(int i=0; i<strip.numPixels(); i++) {
+      y = sin(PI * (float)cycles * (float)(x + i) / (float)strip.numPixels());
+      if(y >= 0.0) {
+        // Peaks of sine wave are white
+        y  = 1.0 - y; // Translate Y to 0.0 (top) to 1.0 (center)
+        r2 = 127 - (byte)((float)(127 - r) * y);
+        g2 = 127 - (byte)((float)(127 - g) * y);
+        b2 = 127 - (byte)((float)(127 - b) * y);
+      } else {
+        // Troughs of sine wave are black
+        y += 1.0; // Translate Y to 0.0 (bottom) to 1.0 (center)
+        r2 = (byte)((float)r * y);
+        g2 = (byte)((float)g * y);
+        b2 = (byte)((float)b * y);
+      }
+      strip.setPixelColor(i, r2, g2, b2);
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+/* Helper functions */
+
+//Input a value 0 to 384 to get a color value.
+//The colours are a transition r - g - b - back to r
+
+uint32_t Wheel(uint16_t WheelPos)
+{
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128; // red down
+      g = WheelPos % 128;       // green up
+      b = 0;                    // blue off
+      break;
+    case 1:
+      g = 127 - WheelPos % 128; // green down
+      b = WheelPos % 128;       // blue up
+      r = 0;                    // red off
+      break;
+    case 2:
+      b = 127 - WheelPos % 128; // blue down
+      r = WheelPos % 128;       // red up
+      g = 0;                    // green off
+      break;
+  }
+  return(strip.Color(r,g,b));
+}
